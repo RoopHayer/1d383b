e493827898d28a645adf5d9d1adaba0d6b645635
Rupinder Hayer <ruphayer@gmail.com>
2022-01-10 14:02:34 -0800

    made amendments, changed ASC to DESC in server, created a copy of messages array in 2 functions, added sort method for messages by createdBy

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -14,6 +14,7 @@ export const addMessageToStore = (state, payload) => {
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
       const convoCopy={...convo};
+      convoCopy.messages = [...convo.messages];
       convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       return convoCopy;
@@ -71,6 +72,7 @@ export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
       const convoCopy={...convo};
+      convoCopy.messages = [...convo.messages];
       convoCopy.messages.push(message);
       convoCopy.latestMessageText = message.text;
       convoCopy.id=message.conversationId;

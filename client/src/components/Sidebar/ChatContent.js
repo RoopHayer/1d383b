6ea580ff0e98612fcd52888150e2a0506cc8f9e7
Rupinder Hayer <ruphayer@gmail.com>
2022-01-13 17:57:23 -0800

    added bold styling to unread msgs, #3

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,8 +1,8 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useMemo } from "react";
 import { Box, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
-import { unReadConversations} from "../../store/unreadCount";
+import { unReadConversations } from "../../store/unreadCount";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,25 +20,33 @@ const useStyles = makeStyles((theme) => ({
     color: "#9CADC8",
     letterSpacing: -0.17,
   },
+  boldText: {
+    fontWeight: "bold",
+    color: "black",
+    letterSpacing: -0.17,
+  },
 }));
 
 const ChatContent = (props) => {
   const classes = useStyles();
   const { conversation, unReadConversations } = props;
   const { latestMessageText, otherUser } = conversation;
-  const {unreadCount} = props;
-
-    const countUnread =()=>{
-    let idx = unreadCount.findIndex((element)=>element.id===conversation.id);
-     if(idx>-1){
-     return unreadCount[idx].count;
-     } 
-     return null;
+  const { unreadCount } = props;
+  const countUnread = () => {
+    let idx = unreadCount.findIndex(
+      (element) => element.id === conversation.id
+    );
+    if (idx > -1) {
+      if (unreadCount[idx].count) {
+      }
+      return unreadCount[idx].count;
     }
-  useEffect( ()=>{
+    return null;
+  };
+  // const count = useMemo(() => countUnread(), []);
+  useEffect(() => {
     unReadConversations(conversation);
-  },[unReadConversations])
-
+  }, [unReadConversations]);
 
   return (
     <Box className={classes.root}>
@@ -46,9 +54,21 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
-          {latestMessageText}<span style={{background:'skyBlue', color:'black',fontSize:'bold', margin:'4rem'}}>
-          { countUnread()}</span>
+        <Typography
+          className={countUnread() ? classes.boldText : classes.previewText}
+        >
+          {latestMessageText}
+          <span
+            style={{
+              background: "skyBlue",
+              color: "black",
+              fontSize: "bold",
+              margin: "4rem",
+              borderRadius: "50%",
+            }}
+          >
+            {countUnread()}
+          </span>
         </Typography>
       </Box>
     </Box>
@@ -57,14 +77,14 @@ const ChatContent = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    unreadCount: state.unreadCount
+    unreadCount: state.unreadCount,
   };
 };
 const mapDispatchToProps = (dispatch) => {
   return {
-    unReadConversations:(messages)=>{
-      dispatch(unReadConversations(messages))
-    }
+    unReadConversations: (messages) => {
+      dispatch(unReadConversations(messages));
+    },
   };
 };
 

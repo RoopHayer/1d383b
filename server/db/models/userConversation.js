c139883a93a3489410cb93b36ef9022d4ba78a6e
Rupinder Hayer <ruphayer@gmail.com>
2022-01-28 22:04:22 -0800

    added userConversation model in db, #5

---
+++ server/db/models/userConversation.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+const User = require("./user");
+const Conversation = require("./conversation");
+
+const UserConversation = db.define("userConversation", {
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: "id",
+    },
+    allowNull: false,
+  },
+  conversationId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Conversation,
+      key: "id",
+    },
+    allowNull: false,
+  },
+});
